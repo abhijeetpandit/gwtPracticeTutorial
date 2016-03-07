2a7de1ccd0ed70de03b468c724f2b64887931fce
apandit <apandit@CAPMPT2040036.CAPMPTDC.COM>
2016-03-07 14:39:10 +0530

    Updated css demo

--- 002-CSSDemo/src/org/abhi/cssdemo/client/_02_CSSDemo.java
+++ 002-CSSDemo/src/org/abhi/cssdemo/client/_02_CSSDemo.java
@@ -3,7 +3,10 @@ package org.abhi.cssdemo.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 
 /**
@@ -31,8 +34,43 @@ public class _02_CSSDemo implements EntryPoint {
 			}
 		});
 		
+		
 		RootPanel.get("greenButton").add(button1);
 		RootPanel.get("redButton").add(button2);
-	}
+		
+		Label blueLabel = new Label("Blue Label");
+		blueLabel.setStylePrimaryName("blue-text");
+		Label blueBoldLabel = new Label("Blue Bold Label");
+		blueBoldLabel.setStylePrimaryName("blue-text");
+		blueBoldLabel.addStyleName("bold-text");
+		RootPanel panel = RootPanel.get("labelsTest");
+		panel.add(blueLabel);
+		panel.add(blueBoldLabel);
 	
+	
+		RootPanel labelsPanel = RootPanel.get("labels-test2");
+		
+		Label label1 = new Label("First Label");
+		Label label2 = new Label("Second Label");
+		
+		label1.setTitle("title1(tooltip1)");
+		label2.setTitle("title2(tooltip2)");
+		label1.setStyleName("style1");
+		label2.setStyleName("style2");
+		
+		labelsPanel.add(label1);
+		labelsPanel.add(label2);
+		
+		HTML html1 = new HTML("HTML text1");
+		HTML html2 = new HTML("HTML text1");
+		
+		html1.setStyleName("style1");
+		html2.setStyleName("style2");
+		
+		RootPanel.get("html-test").add(html1);
+		RootPanel.get("html-test").add(html2);
+		
+		Anchor anchor = new Anchor("Dashboard", false, "http://www.google.co.in", "_blank");
+		RootPanel.get("html-test").add(anchor);
+	}
 }
